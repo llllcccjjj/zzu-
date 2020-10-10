701b685027885139717063ef497093501b907c22
llllcccjjj <70000656+llllcccjjj@users.noreply.github.com>
2020-10-10 14:24:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from selenium import webdriver
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
+import requests
 def dk(user,pas):
     try:
-        # 模拟浏览器打开网站
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
+        option = webdriver.ChromeOptions()
+        option.add_argument('--headless')
+        option.add_argument('--no-sandbox')
+        driver = webdriver.Chrome()
         driver.get("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/first0")
         driver.implicitly_wait(10)
         driver.find_element_by_name('uid').send_keys(user)
@@ -24,10 +20,18 @@ def dk(user,pas):
     except:
         driver.quit()
         print("执行失败!")
+        token = '39ece89f3624407995a957b2d058b4d5' #在push+网站中可以找到
+        title= '打卡执行情况' #改成你要的标题内容
+        content ='执行失败' #改成你要的正文内容
+        url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token+'&title='+title+'&content='+content
+        requests.get(url)
     else:
         driver.quit()
         print("success")
-
-
+        token = '39ece89f3624407995a957b2d058b4d5' #在push+网站中可以找到
+        title= '打卡执行情况' #改成你要的标题内容
+        content ='执行成功' #改成你要的正文内容
+        url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token+'&title='+title+'&content='+content
+        requests.get(url)
 if __name__ == "__main__":
     dk('201677H0323', '08286050')
