a4c38a4a846dd7fc604d4c2800cfe7a1df333b93
912733117 <72393802+912733117@users.noreply.github.com>
2020-10-16 20:28:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import requests
 token=os.environ['PLUS_KEY']
 s_name=os.environ['S_NAME']
 s_pwd=os.environ['S_PWD']
+token1=os.environ['PLUS_KEY1']
+s_name1=os.environ['S_NAME1']
+s_pwd1=os.environ['S_PWD1']
 def dk(user,pas):
     try:
         # 模拟浏览器打开网站
@@ -31,12 +34,16 @@ def dk(user,pas):
         content ='执行失败' #改成你要的正文内容
         url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token+'&title='+title+'&content='+content
         requests.get(url)
+        url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token1+'&title='+title+'&content='+content
+        requests.get(url)
     else:
         driver.quit()
         print("success")
         title= '打卡执行情况' #改成你要的标题内容
         content ='执行成功' #改成你要的正文内容
         url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token+'&title='+title+'&content='+content
+        requests.get(url)
+        url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token1+'&title='+title+'&content='+content
         requests.get(url)
 if __name__ == "__main__":
     dk(s_name, s_pwd)
