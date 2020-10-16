5707a5aed8856f445a699d84d4c0a2ddd8927e03
912733117 <72393802+912733117@users.noreply.github.com>
2020-10-16 20:48:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,6 @@ def dk(user,pas):
         content ='执行失败' #改成你要的正文内容
         url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token+'&title='+title+'&content='+content
         requests.get(url)
-        url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token1+'&title='+title+'&content='+content
-        requests.get(url)
     else:
         driver.quit()
         print("success")
@@ -43,8 +41,39 @@ def dk(user,pas):
         content ='执行成功' #改成你要的正文内容
         url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token+'&title='+title+'&content='+content
         requests.get(url)
+def dk1(user,pas):
+    try:
+        # 模拟浏览器打开网站
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
+        driver.get("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/first0")
+        driver.implicitly_wait(10)
+        driver.find_element_by_name('uid').send_keys(user)
+        driver.find_element_by_name('upw').send_keys(pas)
+        driver.find_element_by_name('smbtn').click()
+        driver.implicitly_wait(10)
+        driver.get(driver.find_element_by_id('zzj_top_6s').get_attribute('src'))
+        driver.find_element_by_xpath('//span[text()="本人填报"]').click()
+        driver.implicitly_wait(10)
+        driver.find_element_by_xpath('//span[text()="提交表格"]').click()
+        print("1111111111111")
+    except:
+        driver.quit()
+        print("执行失败!")
+        title= '打卡执行情况' #改成你要的标题内容
+        content ='执行失败' #改成你要的正文内容
         url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token1+'&title='+title+'&content='+content
         requests.get(url)
+    else:
+        driver.quit()
+        print("success")
+        title= '打卡执行情况' #改成你要的标题内容
+        content ='执行成功' #改成你要的正文内容
+        url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token1+'&title='+title+'&content='+content
+        requests.get(url)        
 if __name__ == "__main__":
     dk(s_name, s_pwd)
-    dk(s_name1,s_pwd1)
+    dk1(s_name1,s_pwd1)
