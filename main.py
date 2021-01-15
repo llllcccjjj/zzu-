ba8248715237c1a2acda53552ada3324076da6a4
llllcccjjj <70000656+llllcccjjj@users.noreply.github.com>
2021-01-15 09:49:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,15 +26,15 @@ def dk(user,pas):
     except:
         driver.quit()
         print("执行失败!")
-        title= '打卡执行情况' #改成你要的标题内容
-        content = name+'打卡失败'#改成你要的正文内容
+        title= name+'同学打卡执行情况' #改成你要的标题内容
+        content = '打卡失败'#改成你要的正文内容
         url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token+'&title='+title+'&content='+content
         requests.get(url)
     else:
         driver.quit()
         print("success")
-        title= '打卡执行情况' #改成你要的标题内容
-        content = name+'打卡成功' #改成你要的正文内容
+        title= name+'同学打卡执行情况' #改成你要的标题内容
+        content = '打卡成功' #改成你要的正文内容
         url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token+'&title='+title+'&content='+content
         requests.get(url)
 
